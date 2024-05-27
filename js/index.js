dcafb80f4a31654849d39129d4e9b53368e29971
alfreddai <ya95123@gmail.com>
2024-05-27 17:44:51 +0800

    test: banner bg effect

--- js/index.js
+++ js/index.js
@@ -38,7 +38,6 @@ anime({
 let titleSetting = {
   delay: {
     start: loadingSetting.entry.duration + loadingSetting.leave.duration,
-    // start: 0,
     stagger: 500,
   },
   duration: 2000,
@@ -53,4 +52,32 @@ anime({
   }),
   duration: titleSetting.duration,
   easing: titleSetting.easing,
+});
+
+// banner meteor
+let meteorSetting = {
+  delay: {
+    // start: titleSetting.delay.start + titleSetting.duration,
+    start: 0,
+    stagger: 2000,
+  },
+  duration: 1000,
+  easing: "easeInOutQuad",
+};
+
+// TODO 各自分開結束 & loop / 換背景
+anime({
+  targets: ".banner__meteor",
+  height: [anime.random(40, 60), anime.random(90, 100)],
+  translateX: [0, -100],
+  translateY: [0, 60],
+  rotate: [60, 60],
+  opacity: [0, 1],
+  delay: anime.stagger(meteorSetting.delay.stagger, {
+    start: meteorSetting.delay.start,
+  }),
+  duration: meteorSetting.duration,
+  easing: meteorSetting.easing,
+  // direction: "alternate",
+  loop: true,
 });
