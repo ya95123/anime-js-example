cd8c8263bc5e5091fd8c854efd634884514fab76
alfreddai <ya95123@gmail.com>
2024-05-27 15:45:26 +0800

    feat: banner title

--- js/index.js
+++ js/index.js
@@ -1,28 +1,56 @@
 // banner loading
 let loadingSetting = {
-  duration: 2000,
-  easing: "easeInOutQuint",
+  entry: {
+    duration: 2000,
+    easing: "easeInOutQuint",
+  },
+  leave: {
+    delay: 2000,
+    duration: 500,
+    easing: "easeInOutQuint",
+  },
 };
 
 anime({
   targets: ".banner__loading-bar",
   width: "100%",
-  duration: loadingSetting.duration,
-  easing: loadingSetting.easing,
+  duration: loadingSetting.entry.duration,
+  easing: loadingSetting.entry.easing,
 });
 
 anime({
   targets: ".banner__loading-num",
   innerText: "100 %",
   round: 1,
-  duration: loadingSetting.duration,
-  easing: loadingSetting.easing,
+  duration: loadingSetting.entry.duration,
+  easing: loadingSetting.entry.easing,
 });
 
 anime({
   targets: ".banner__loading",
   opacity: 0,
-  delay: loadingSetting.duration,
-  duration: 500,
-  easing: loadingSetting.easing,
+  delay: loadingSetting.leave.delay,
+  duration: loadingSetting.leave.duration,
+  easing: loadingSetting.leave.easing,
+});
+
+// banner title
+let titleSetting = {
+  delay: {
+    start: loadingSetting.entry.duration + loadingSetting.leave.duration,
+    // start: 0,
+    stagger: 500,
+  },
+  duration: 2000,
+  easing: "easeInOutSine",
+};
+
+anime({
+  targets: ".banner__title path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  delay: anime.stagger(titleSetting.delay.stagger, {
+    start: titleSetting.delay.start,
+  }),
+  duration: titleSetting.duration,
+  easing: titleSetting.easing,
 });
