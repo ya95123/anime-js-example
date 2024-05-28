3bf2247628337f3de033d0456522ce8170d4efd9
alfreddai <ya95123@gmail.com>
2024-05-28 11:08:08 +0800

    perf: let anime has better timeline

--- js/index.js
+++ js/index.js
@@ -1,58 +1,29 @@
-// banner loading
-let loadingSetting = {
-  entry: {
+let bannerAnime = anime
+  .timeline({
+    targets: ".banner__loading-bar",
+    width: "100%",
     duration: 2000,
     easing: "easeInOutQuint",
-  },
-  leave: {
-    delay: 2000,
+  })
+  .add(
+    {
+      targets: ".banner__loading-num",
+      innerText: "100 %",
+      round: 1,
+    },
+    0
+  )
+  .add({
+    targets: ".banner__loading",
+    opacity: 0,
     duration: 500,
-    easing: "easeInOutQuint",
-  },
-};
-
-anime({
-  targets: ".banner__loading-bar",
-  width: "100%",
-  duration: loadingSetting.entry.duration,
-  easing: loadingSetting.entry.easing,
-});
-
-anime({
-  targets: ".banner__loading-num",
-  innerText: "100 %",
-  round: 1,
-  duration: loadingSetting.entry.duration,
-  easing: loadingSetting.entry.easing,
-});
-
-anime({
-  targets: ".banner__loading",
-  opacity: 0,
-  delay: loadingSetting.leave.delay,
-  duration: loadingSetting.leave.duration,
-  easing: loadingSetting.leave.easing,
-});
-
-// banner title
-let titleSetting = {
-  delay: {
-    start: loadingSetting.entry.duration + loadingSetting.leave.duration,
-    stagger: 500,
-  },
-  duration: 2000,
-  easing: "easeInOutSine",
-};
-
-anime({
-  targets: ".banner__title path",
-  strokeDashoffset: [anime.setDashoffset, 0],
-  delay: anime.stagger(titleSetting.delay.stagger, {
-    start: titleSetting.delay.start,
-  }),
-  duration: titleSetting.duration,
-  easing: titleSetting.easing,
-});
+  })
+  .add({
+    targets: ".banner__title path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    delay: anime.stagger(500),
+    easing: "easeInOutSine",
+  });
 
 // banner meteor
 let meteorSetting = {
@@ -66,18 +37,18 @@ let meteorSetting = {
 };
 
 // TODO 各自分開結束 & loop / 換背景
-anime({
-  targets: ".banner__meteor",
-  height: [anime.random(40, 60), anime.random(90, 100)],
-  translateX: [0, -100],
-  translateY: [0, 60],
-  rotate: [60, 60],
-  opacity: [0, 1],
-  delay: anime.stagger(meteorSetting.delay.stagger, {
-    start: meteorSetting.delay.start,
-  }),
-  duration: meteorSetting.duration,
-  easing: meteorSetting.easing,
-  // direction: "alternate",
-  loop: true,
-});
+// anime({
+//   targets: ".banner__meteor",
+//   height: [anime.random(40, 60), anime.random(90, 100)],
+//   translateX: [0, -100],
+//   translateY: [0, 60],
+//   rotate: [60, 60],
+//   opacity: [0, 1],
+//   delay: anime.stagger(meteorSetting.delay.stagger, {
+//     start: meteorSetting.delay.start,
+//   }),
+//   duration: meteorSetting.duration,
+//   easing: meteorSetting.easing,
+//   // direction: "alternate",
+//   loop: true,
+// });
